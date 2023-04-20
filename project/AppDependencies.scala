1e86ad6d063092683cd54bb459bda0003e62340a
Tony Ogilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-04-20 14:10:28 +0100

    Updated dependencies (#3)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,14 @@ import sbt._
 
 object AppDependencies {
 
+  private val hmrcBootstrapVersion = "7.15.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.12.0"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28"   % "7.12.0",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
     "org.mockito"         %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus"   %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola" %% "random-data-generator"    % "2.9"
