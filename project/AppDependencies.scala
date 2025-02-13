8e6cd58d4b3774e6288a6762fbe6ca2bbdc5a956
Athish Thayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-02-13 11:26:10 +0000

    DDCYLS-5995 - Dependency upgrades (#13)
    * dependency upgrades
    
    * coverage upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.0.0"
+  private val hmrcBootstrapVersion = "9.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % hmrcBootstrapVersion
