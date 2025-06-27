d8f67bf913aa00de96b747a42c5a51fa7eaac61a
VJWatson <171040641+VJWatson@users.noreply.github.com>
2025-06-27 14:33:23 +0100

    app deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.11.0"
+  private val hmrcBootstrapVersion = "9.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % hmrcBootstrapVersion
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
-    "org.mockito"         %% "mockito-scala"            % "1.17.30",
+    "org.mockito"         %% "mockito-scala"            % "2.0.0",
     "org.scalatestplus"   %% "scalacheck-1-17"          % "3.2.18.0",
     "com.danielasfregola" %% "random-data-generator"    % "2.9"
   ).map(_ % Test)
