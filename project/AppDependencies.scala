d669a0f07f0405a04423eba532d0d981a8e0f218
Bharti Verma <68339135+bharti5verma@users.noreply.github.com>
2026-03-13 15:27:25 +0000

    DDCYLS-8210 Vulnerability fix (#21)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "10.5.0"
+  private val hmrcBootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % hmrcBootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % hmrcBootstrapVersion,
+    "ch.qos.logback" % "logback-core"               % "1.5.27",
+    "ch.qos.logback" % "logback-classic"            % "1.5.27",
+    "org.apache.commons" % "commons-lang3"          % "3.18.0",
+    "at.yawk.lz4"    % "lz4-java"                   % "1.10.3"
   )
 
   val test: Seq[ModuleID]    = Seq(
